5d7d22a8a07ee6fdd9efdda79fe24827a66cb622
armando.ferrara <armando.ferrara@dormakaba.com>
2022-05-19 17:38:51 +0200

    circleCI updated based on parent BOM dependency

--- mssc-beer-order/src/main/java/sfg/beer/order/service/bootstrap/BeerOrderBootStrap.java
+++ mssc-beer-order/src/main/java/sfg/beer/order/service/bootstrap/BeerOrderBootStrap.java
@@ -26,7 +26,7 @@ public class BeerOrderBootStrap implements CommandLineRunner {
     }
 
     private void loadCustomerData() {
-        if (customerRepository.findAllByCustomerNameLike(BeerOrderBootStrap.TASTING_ROOM) .size() == 0) {
+        if (customerRepository.findAllByCustomerNameLike(BeerOrderBootStrap.TASTING_ROOM).size() == 0) {
             Customer savedCustomer = customerRepository.saveAndFlush(Customer.builder()
                     .customerName(TASTING_ROOM)
                     .apiKey(UUID.randomUUID())
