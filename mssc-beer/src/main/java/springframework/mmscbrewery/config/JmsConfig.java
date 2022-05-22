393204f4de5a708bada34f8379316740ff56f722
armando.ferrara <armando.ferrara@dormakaba.com>
2022-05-22 18:36:16 +0200

    BOM edited to support JMS, service's properties to communicate with activemq added to each service

---
+++ mssc-beer/src/main/java/springframework/mmscbrewery/config/JmsConfig.java
@@ -0,0 +1,17 @@
+package springframework.mmscbrewery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+    @Bean // Serialize message content to json using TextMessage
+    public MappingJackson2MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
