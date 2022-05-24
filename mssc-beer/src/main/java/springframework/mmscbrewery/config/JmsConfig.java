bc4bb84c0d6976a1bdabde9041e75105bc7242dc
armando.ferrara <armando.ferrara@dormakaba.com>
2022-05-24 17:10:30 +0200

    listener onb mssc-beer service for the validation order added

--- mssc-beer/src/main/java/springframework/mmscbrewery/config/JmsConfig.java
+++ mssc-beer/src/main/java/springframework/mmscbrewery/config/JmsConfig.java
@@ -11,6 +11,8 @@ public class JmsConfig {
 
     public static final String BREWING_REQUEST_QUEUE = "brewing-request";
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
 
     @Bean // Serialize message content to json using TextMessage
     public MappingJackson2MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
