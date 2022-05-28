7882c311aa9da3fe7f4d0e24a79ea32577511fed
armando.ferrara <armando.ferrara@dormakaba.com>
2022-05-28 10:12:32 +0200

    eureka client applied to inventory and order service

--- mssc-beer/src/main/java/springframework/mmscbrewery/MmscBreweryApplication.java
+++ mssc-beer/src/main/java/springframework/mmscbrewery/MmscBreweryApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MmscBreweryApplication {
 
 	public static void main(String[] args) {
