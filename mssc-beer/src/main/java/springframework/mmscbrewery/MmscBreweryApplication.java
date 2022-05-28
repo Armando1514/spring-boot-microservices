9630f8cea9fe89e22d02435b91f3bfaa7014de3a
armando.ferrara <armando.ferrara@dormakaba.com>
2022-05-28 15:23:29 +0200

    Service discovery with Eureka added

--- mssc-beer/src/main/java/springframework/mmscbrewery/MmscBreweryApplication.java
+++ mssc-beer/src/main/java/springframework/mmscbrewery/MmscBreweryApplication.java
@@ -3,7 +3,9 @@ package springframework.mmscbrewery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MmscBreweryApplication {
 
