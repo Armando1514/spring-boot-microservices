4587ebe6f14ce1c947738ba6fd83ff6340c31a3d
armando.ferrara <armando.ferrara@dormakaba.com>
2022-05-29 15:11:28 +0200

    first configuration for mssc-config-server

--- mssc-config-server/src/main/java/mssc/msscconfigserver/MsscConfigServerApplication.java
+++ mssc-config-server/src/main/java/mssc/msscconfigserver/MsscConfigServerApplication.java
@@ -2,7 +2,9 @@ package mssc.msscconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class MsscConfigServerApplication {
 
