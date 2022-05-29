4807f30d42e49bdd50224871b7709313cf65da39
armando.ferrara <armando.ferrara@dormakaba.com>
2022-05-29 14:24:42 +0200

    initial config server project

---
+++ mssc-config-server/src/main/java/mssc/msscconfigserver/MsscConfigServerApplication.java
@@ -0,0 +1,13 @@
+package mssc.msscconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscConfigServerApplication.class, args);
+    }
+
+}
---
+++ mssc-config-server/src/test/java/mssc/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mssc.msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
